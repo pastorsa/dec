40d505dc37bd981f0991763aff4398ae4be12611
Peter Pastor <pastorsa@usc.edu>
2013-08-14 01:49:57 -0700

    debugging

--- dec_teensy/dec_teensy/node_info.h
+++ dec_teensy/dec_teensy/node_info.h
@@ -1,7 +1,7 @@
 #ifndef NODE_INDE_H_
 #define NODE_INDE_H_
-#define NODE_ID 1
-#define NODE_ID_HEX 0x01
-#define NODE_NET 0
-#define NODE_NET_HEX 0x00
+#define NODE_ID 2
+#define NODE_ID_HEX 0x02
+#define NODE_NET 1
+#define NODE_NET_HEX 0x01
 #endif
