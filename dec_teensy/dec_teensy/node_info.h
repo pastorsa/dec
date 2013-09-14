338f91bdbc8c760bd8cb6c145228ce618a58d41d
Peter Pastor <pastorsa@usc.edu>
2013-09-13 20:18:33 -0700

    Burn it

--- dec_teensy/dec_teensy/node_info.h
+++ dec_teensy/dec_teensy/node_info.h
@@ -1,10 +1,10 @@
 #ifndef NODE_INDE_H_
 #define NODE_INDE_H_
 
-// Setting ip to 10.0.1.3
+// Setting ip to 10.0.1.4
 
-#define NODE_ID 3
-#define NODE_ID_HEX 0x03
+#define NODE_ID 4
+#define NODE_ID_HEX 0x04
 
 #define NODE_NET 0x01
 
